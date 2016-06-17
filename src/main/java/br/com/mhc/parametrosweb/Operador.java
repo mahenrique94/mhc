3bfc3d12355db2f5a18ba01ff742ab39e62a9875
Matheus <matheushcastiglioni@gmail.com>
2016-06-17 00:37:07 -0300

    Efetuado correções no ParametrosWeb

--- src/main/java/br/com/mhc/parametrosweb/Operador.java
+++ src/main/java/br/com/mhc/parametrosweb/Operador.java
@@ -5,8 +5,10 @@ import java.util.List;
 public interface Operador {
 
 	default String get(List<String> parametro, String operador, String type) {
-		if (type.equals("String"))
+		if (type.equals("String") && operador.endsWith("like"))
 			return operador + " '%" + parametro.get(1) + "%'";
+		if (type.equals("String") && !operador.endsWith("like"))
+			return operador + " '" + parametro.get(1) + "'";
 		return operador + " " + parametro.get(1);
 	};
 	
