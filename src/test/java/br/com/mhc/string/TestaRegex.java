e499da32a51d4cb8e74a1cc8fdb3c939c652f25e
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-09-04 19:58:22 -0300

    Updating repo

--- src/test/java/br/com/mhc/string/TestaRegex.java
+++ src/test/java/br/com/mhc/string/TestaRegex.java
@@ -1,10 +1,21 @@
 package br.com.mhc.string;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TestaRegex {
 
 	public static void main(String[] args) {
-		String s = "4630 - MOD. 15 S/N' A'";
-		System.out.println(s.replaceAll("(([^0-9])([^S\\/N\\']?))", ""));
+		StringBuilder resultado = new StringBuilder();
+		String s = "<?xml version=\"1.0\"?><data><item0><chave>35170908676162000156570010000236191000489454</chave><protocolo>0588699990867616200015657001000023619086</protocolo></item0></data>";
+		String r = "((<chave.+?>)|(<protocolo.+?>))";
+		Pattern pattern = Pattern.compile(r);
+		Matcher matcher = pattern.matcher(s);
+		while (matcher.find()) {
+			resultado.append(matcher.group());
+			resultado.append("\n");
+		}
+		System.out.println(resultado.toString());
 	}
 	
 }
---
+++ src/test/java/br/com/mhc/tdd/For.java
@@ -0,0 +1,13 @@
+package br.com.mhc.tdd;
+
+public class For {
+
+    public static void main(String[] args) {
+        int x[] = new int[3];
+        for(x[1] = x.length-1; x[0] == 0; x[1]--) {
+            x[x[1]] = -5;
+            System.out.println(x[1]);
+        }
+    }
+
+}
